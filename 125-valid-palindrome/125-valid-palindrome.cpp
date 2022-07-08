65d430b31c3ec248d9d18cb1c4ec7968975784b6
Aarushi2812 <98702210+Aarushi2812@users.noreply.github.com>
2022-07-09 00:52:33 +0530

    Time: 7 ms (68.41%), Space: 7.8 MB (23.38%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-bool isPalindrome(string s) {
-string temp="";
-for(int i=0;i<s.size();i++){
-if(s[i]>=65&&s[i]<=90)temp+=(s[i]+32);
-if(s[i]>=97&&s[i]<=122 || s[i]>='0'&&s[i]<='9')temp+=s[i];
-}
-cout<<temp;
-int i=0,e=temp.size()-1;
-while(i<e){
-if(temp[i]!=temp[e])return false;
-i++;
-e--;
-}
-return true;
-}
+    bool isPalindrome(string s) {      
+            string c=""; 
+            for(int i=0; i<s.length() ; i++)
+            {
+                    if((s[i]>='a' and s[i]<='z')||(s[i]>='0' and s[i]<='9'))
+                    {
+                     c+=s[i];   
+                    }
+                    else if(s[i]>='A' and s[i]<='Z')
+                    {
+                            c+=(s[i]+32);
+                    }            
+            }
+       //  int e=c.length()-1;
+          int  i=0;
+            for(i=0; i<c.length()/2 ; i++){
+                 if(c[i]!=c[c.length()-i-1]){
+                         return false;
+                 } 
+                   
+                           }         
+                    return true;
+    }
 };
+
+
+// class Solution {
+// public:
+// bool isPalindrome(string s) {
+// string temp="";
+// for(int i=0;i<s.size();i++){
+// if(s[i]>=65&&s[i]<=90)temp+=(s[i]+32);
+// if(s[i]>=97&&s[i]<=122 || s[i]>='0'&&s[i]<='9')temp+=s[i];
+// }
+// cout<<temp;
+// int i=0,e=temp.size()-1;
+// while(i<e){
+// if(temp[i]!=temp[e])return false;
+// i++;
+// e--;
+// }
+// return true;
+// }
+// };
