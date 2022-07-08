fc39bbf293b8f9d45f2982a9b28cf8f54570a500
Aarushi2812 <98702210+Aarushi2812@users.noreply.github.com>
2022-07-08 23:54:14 +0530

    Time: 757 ms (5.63%), Space: 23.9 MB (43.19%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    bool validPalindrome(string s) {
-        int n= s.length();
-        // i and j are two pointers to check palindrome
-        int i=0, j=n-1;
+    bool check(int i, int j,string s){
+          cout<<i<<j<<endl;
+            while(i<=j){
+                    if(s[i]==s[j]){
+                            i++;
+                            j--;
+                    }
+                    else 
+                    {
+                            return false;
+                    }
+            }
+            return true;
+            }
         
-        while(i<=j){
-            if(s[i]!=s[j]) break;
-            i++; j--;
-        }
-        if(i>j) return true;//already palindrme
-        
-        //not palindrome so
-        //delete char at i(i.e. d0 i++) and check whether it beomes palindrome
-        int newi=i+1, newj=j;
-        while(newi<=newj){
-            if(s[newi]!=s[newj])  break;
-            newi++; newj--;
-        }
-        if(newi>newj) return true; 
-        
-        //delete char at j(i.e. do j--) and check whether it beomes palindrome
-        newi=i; newj=j-1;
-        while(newi<=newj){
-            if(s[newi]!=s[newj])  return false;
-            newi++; newj--;
-        }
-        return true;
+        bool validPalindrome(string s) {        
+     int j=s.length()-1;
+                for(int i=0; j>i; ){
+                        if(s[i]==s[j]){cout<<"g"<<endl;
+                                j--;
+                                i++;
+                        }
+                        else {
+                         if(check(i+1,j,s)){cout<<"a"<<endl;
+                                 return true;
+                         }
+                        else if(check(i,j-1,s)){cout<<"b"<<endl;
+                           return true;     
+                        }
+                        else{cout<<"c"<<endl;
+                                return false;
+                        }
+                        }
+                        
+                }
+                
+         return true;   
+           
     }
 };
