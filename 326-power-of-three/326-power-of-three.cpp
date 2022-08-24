525115f2795b3ebe5cce347dec96c74865aadcc8
Aarushi2812 <98702210+Aarushi2812@users.noreply.github.com>
2022-08-25 00:05:44 +0530

    Time: 54 ms (11.56%), Space: 5.9 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-        if(n==0){
-                return false;
-        }
-            while(n!=1){
-                    if(n%2!=0){
-                            return false;
-                    }
-                    n=n/2;
-            }
-       return true;    
-    } 
+    bool isPowerOfThree(int n) {
+       // if(n==0){
+       //          return false;
+       //  }
+       //      while(n!=1){
+       //              if(n%3!=0){
+       //                      return false;
+       //              }
+       //              n=n/3;
+       //      }
+       // return true; 
+            int a=pow(3,19);
+            //cout<<a<<endl;
+            return n>0 and a%n==0;
+            
+    }
 };
