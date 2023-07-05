6c7c0e98672477f0955dd57d36aa355050993ee2
Aarushi2812 <98702210+Aarushi2812@users.noreply.github.com>
2023-07-05 12:50:59 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+     vector<int> shortestPath(int n,int m , vector<vector<int>>& edges){
+        // code here
+        vector<int> ans(n, 100000009);
+        ans[0] = 0;
+        // making adj
+        vector<vector<pair<int, int>>> adj(n, vector<pair<int, int>>());
+        for(int i = 0; i<m; i++){
+            adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+        }
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        while(q.size()!=0){
+            int node = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+            //cout<<node <<" "<<dist<<endl;
+            for(auto i: adj[node]){
+                int subnode = i.first;
+                int subdist = i.second + dist;
+                if(ans[subnode] > subdist){
+                    ans[subnode] = subdist;
+                    q.push({subnode, subdist});
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            //cout<<ans[i]<<endl;
+            if(ans[i]==100000009){
+                ans[i] = -1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
